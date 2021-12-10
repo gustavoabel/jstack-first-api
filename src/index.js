b337140258b66efca0ea83a1672f06e08141618c
gustavoabell <gustavoabel.contato@gmail.com>
2021-12-10 10:28:20 -0300

    create initial endpoints

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
+const users = require('./mocks/users');
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('<h1>Hello World!</h1>')
+  console.log(`Request method: ${request.method}) | Endpoint: ${request.url}`);
+
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
+
+  }
+
 
 });
 
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    "id": 1,
+    "name": "Gustavo"
+  },
+  {
+    "id": 2,
+    "name": "Gabriel"
+  },
+  {
+    "id": 3,
+    "name": "Felipe"
+  }
+]
