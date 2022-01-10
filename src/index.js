91540fb8248889e79cb926f2981fa43cceea4038
gustavoabell <gustavoabel.contato@gmail.com>
2022-01-10 19:51:35 -0300

    create controller of users and system of routes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const http = require('http');
 
-const users = require('./mocks/users');
+const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method}) | Endpoint: ${request.url}`);
 
-
-  if (request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(users));
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
+  const route = routes.find((routeObj) =>  (
+    routeObj.endpoint === request.url && routeObj.method === request.method 
+  ));
+    if (route) {
+      route.handler(request, response);
+    } else {
+      response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end(`Cannot ${request.method} ${request.url}`);
-
-  }
-
-
+    }
+  // if (request.url === '/users' && request.method === 'GET') {
+  //   UserController.listUsers(request, response);
+  // } else {
+     
+  //}
 });
 
 server.listen(3000, () => console.log('💥 Server started at http://localhost:3000'))
